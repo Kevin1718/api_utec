7c12c6191611b8a7158d2493ca0d0f96726de727
Kevin1718 <51490042+Kevin1718@users.noreply.github.com>
2020-03-30 11:48:13 -0600

    api

---
+++ apiuniversidad_final-master/app.py
@@ -0,0 +1,13 @@
+import web # pip install web.py
+from web.template import ALLOWED_AST_NODES
+ALLOWED_AST_NODES.append ('Constant')
+
+urls = (
+    '/alumnos?', 'application.controllers.alumnos.ArchivoUtec',
+)
+app = web.application(urls, globals())
+
+
+if __name__ == "__main__":
+    web.config.debug = False
+    app.run()
